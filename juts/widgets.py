1f4c9a78fc86d6069868e33b21cdde68b5757996
Marcus Riesmeier <marcus.riesmeier@umit.at>
2019-05-25 18:52:29 +0200

    Avoid juts widget

--- juts/widgets.py
+++ juts/widgets.py
@@ -2,32 +2,15 @@ from IPython.display import display
 import ipywidgets as iw
 
 
-class JutsWidget:
-    def __init__(self, widget):
-        self.widget = widget
-
-    def get_widget(self):
-        return self.widget
-
-    def set_layout(self, layout):
-        self.widget.layout = layout
-
-    def _repr_pretty_(self, *args):
-        display(self.widget)
-
-
-class LabeledList(JutsWidget):
+class SelectWithLabel(iw.VBox):
     def __init__(self, label, list_items):
         self.label = iw.Label(label)
         self.config_select_view = iw.Select(options=list_items)
-
-        widget = iw.VBox(children=[self.label, self.config_select_view])
-        super(LabeledList, self).__init__(widget)
+        super(SelectWithLabel, self).__init__(children=[self.label, self.config_select_view])
 
 
-class ConfigurationView(JutsWidget):
-    def __init__(self):
-
+class AccordionWithHeader(iw.Accordion):
+    def __init__(self, header, labeled):
         widget = iw.VBox(children=[])
-        super(ConfigurationView, self).__init__(widget)
+        super(AccordionWithHeader, self).__init__(widget)
 
